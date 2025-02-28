08c2e35b052a87e27720c4883b3d4d68acb4b820
Saul van der Walt <10322221+SaulDoesCode@users.noreply.github.com>
2025-02-28 09:14:15 +0200

    Create main.m

---
+++ main.m
@@ -0,0 +1,37 @@
+50.5 #a
+50.5 #b
++ a b
+# c
+
+> `~a + ~b = ~c`
+
+2.10 #a
+10.2 #b
+** a b
+# c
+
+> `~a + ~b = ~c`
+
+> `okay`
+
+`yes` moo
+`flowrs can be eaten and are sometimes tasty` yum
+> `there is moo? ~moo`
+
+`hoaw wurl and uuuuu ~moo, also ~yum` s
+`yep` msg
+
+`> ^~s^ 
+^yep^ moo` rep
+
+
+~ hi `~rep`
+
+r 2 hi
+
+> `there is moo? ~moo`
+
+`rainbow` gravity
+
+< `echo hi`
+> `~r`
